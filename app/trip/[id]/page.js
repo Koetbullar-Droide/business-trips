3026b00dcef07fd624532b2cea2bd33036032f88
Jannik Luethi <jannik.luethi@swisscom.com>
2024-07-11 20:29:05 +0200

    Pie Chart

--- app/trip/[id]/page.js
+++ app/trip/[id]/page.js
@@ -15,6 +15,9 @@ import Card from '@mui/joy/Card';
 import MonetizationOnRoundedIcon from '@mui/icons-material/MonetizationOnRounded';
 import { PieChart } from '@mui/x-charts/PieChart';
 import Slider from '@mui/joy/Slider';
+import FormControl from '@mui/joy/FormControl';
+import FormLabel from '@mui/joy/FormLabel';
+import Alert from '@mui/joy/Alert';
 
 
 
@@ -30,8 +33,31 @@ export default function Trip({ params }) {
     // Assuming you have a trips array in the Home component
 
     const [trip, setTrip] = useState();
-    
+    const [budgetLine, setBudgetLine] = useState(false);
 
+    const [sliderData, setSliderData] = useState({
+        accomodation: 500,
+        food: 850,
+        transport: 500
+      });
+
+      const handleChange = (e) => {
+        const { name, value } = e.target;
+        setSliderData({
+          ...sliderData,
+          [name]: value
+        });
+      };
+
+    useEffect(() => {
+        if( sliderData.accomodation + sliderData.food + sliderData.transport > trip?.budget){
+            setBudgetLine(true);
+        } else {
+            setBudgetLine(false);
+        }
+    }, [ sliderData, trip?.budget]);
+    
+    
     useEffect(() => {
         fetch('/api/'+params.id)
             .then(response => response.json())
@@ -117,18 +143,30 @@ export default function Trip({ params }) {
                                     lg: 'h1'   // style for medium and larger screens
                                     } 
                                 }} variant="soft">The Budget in detail</Typography>
+                                
+                        </div>
+                        <div className=' w-full mt-5'>
+                            <Typography 
+                            level='title-lg'
+                            >Budget: {trip.budget} CHF</Typography>
+
+                                <Typography  
+                                level='title-lg'
+                                color={budgetLine ? 'danger' : 'success'}
+                                variant="plain"
+                                >Current Cost: {sliderData.transport + sliderData.accomodation + sliderData.food} CHF</Typography>
                         </div>
 
-                        <div className=' w-full'>
-                            <div className=' w-96'>
+                        <div className=' w-full flex justify-evenly mt-20 items-center'>
+                            <div className=' w-[30rem] '>
 
                                 <PieChart
                                 series={[
                                     {
                                         data: [ 
-                                            { id: 0, value: 10, label: 'series A' },
-                                            { id: 1, value: 15, label: 'series B' },
-                                            { id: 2, value: 20, label: 'series C' },
+                                            { id: 0, value: sliderData.food, label: 'Food' },
+                                            { id: 1, value: sliderData.transport, label: 'Transport' },
+                                            { id: 2, value: sliderData.accomodation, label: 'Accomedation' },
                                         ],
                                         highlightScope: { faded: 'global', highlighted: 'item' },
                                         faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
@@ -136,8 +174,8 @@ export default function Trip({ params }) {
                                         outerRadius: 100,
                                         paddingAngle: 5,
                                         cornerRadius: 5,
-                                        startAngle: -90,
-                                        endAngle: 180,
+                                        startAngle: 0,
+                                        endAngle: 360,
                                         cx: 150,
                                         cy: 150,
                                     }
@@ -145,16 +183,68 @@ export default function Trip({ params }) {
                                 {...pieParams}
                                 />
                             </div>
-                            <div>
-                            <Slider
-                                aria-label="Small steps"
-                                defaultValue={500}
-                                getAriaValueText={valueText}
-                                step={100}
-                                min={0}
-                                max={10000}
-                                valueLabelDisplay="auto"
-                                />
+                            <div className=' w-80'>
+                                <FormControl>
+
+                                    <FormLabel>Accomedation <p className=' text-xl'>🛏️</p></FormLabel> 
+                                    <Slider
+                                        name='accomodation'
+                                        onChange={handleChange}
+                                        value={sliderData.accomodation}
+                                        aria-label="Small steps"
+                                        defaultValue={500}
+                                        getAriaValueText={valueText}
+                                        step={trip?.budget / 100}
+                                        min={0}
+                                        max={trip?.budget}
+                                        valueLabelDisplay="on"
+                                        sx={{ selectedColor: 'success.700'}}
+
+                                        />
+                                
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel>Food<p className=' text-xl'>🥡</p> </FormLabel> 
+                                    <Slider
+                                        name='food'
+                                        onChange={handleChange}
+                                        value={sliderData.food}
+                                        aria-label="Small steps"
+                                        defaultValue={500}
+                                        getAriaValueText={valueText}
+                                        step={trip?.budget / 100}
+                                        min={0}
+                                        max={trip?.budget}
+                                        valueLabelDisplay="auto"
+                                        />
+                                
+                                </FormControl>
+                                <FormControl>
+                                    <FormLabel>Transport <p className=' text-xl'>✈️</p></FormLabel> 
+                                    <Slider
+                                        name='transport'
+                                        onChange={handleChange}
+                                        value={sliderData.transport}
+                                        aria-label="Small steps"
+                                        defaultValue={500}
+                                        getAriaValueText={valueText}
+                                        step={trip?.budget / 100}
+                                        min={0}
+                                        max={trip?.budget}
+                                        valueLabelDisplay="auto"
+                                        />
+                                
+                                </FormControl>
+                               {budgetLine ? (
+                                <Alert
+                                color="danger"
+                                size="md"
+                                variant="outlined"
+                                >
+                                You are over the budget
+                                </Alert>) : null
+                            }
+                                
                             </div>
                         </div>
                         
